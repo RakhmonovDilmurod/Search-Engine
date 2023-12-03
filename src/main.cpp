380fd6dd9affe564fe29a0b707d586cc50408701
RakhmonovDilmurod <152321064+RakhmonovDilmurod@users.noreply.github.com>
2023-12-03 11:36:47 +0500

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "ConvertJSON.h"
+#include "InvertedIndex.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <vector>
+#include <gtest/gtest.h>
+#include "SearchServer.h"
+
+
+int main(int argc,char** argv){
+        ConverterJSON conv;
+        std::map<std::string, std::vector<Entry>> freqDictionary;
+        std::vector<std::string> texts;
+        std::vector<std::string> requests;
+        InvertedIndex inv;
+        inv.UpdateDocumentBase(conv.GetTextDocuments());
+        try {
+            SearchServer searchSer(inv);
+            searchSer.search(searchSer.requestsInput());
+            texts = conv.GetTextDocuments();
+            requests = conv.GetRequests();
+            conv.putAnswers({{{2,5.34},{8,12.7}},{{1,7.67},{2,34.67},{3,6.4}}});
+        } catch (OpeningError ex) {
+            std::cerr << ex.what() << std::endl;
+            return 1;
+        } catch (JsonFileContainingError ex) {
+            std::cerr << ex.what() << std::endl;
+            return 1;
+        }
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+
+        return 0;
+    }
